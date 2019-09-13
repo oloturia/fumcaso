2cf8315c58e067a991eecae31ceff0eb19d9d5d8
oloturia <oloturia@gmail.com>
2019-09-13 02:01:21 +0200

    Separated 1 character text from 2 character text The first two elements, or four elements, after the index are the coords of where the text is written Added multiple lines

--- randstrip.py
+++ randstrip.py
@@ -7,11 +7,22 @@ import random
 
 def fetchText(indText):
 	with open("rtext.csv") as rtext:
-		csvReader = csv.reader(rtext)
+		csvReader = csv.reader(rtext,delimiter=';')
 		for row in csvReader:
-			if (row[0]==indText):
-				return row[random.randint(1,len(row)-1)] 
+			if row[0]==indText:
+				return row[1],row[2],row[random.randint(3,len(row)-1)].replace('@','\n')
 				
+def fetch2Text(indText):
+		with open("r2text.csv") as rtext:
+			csvReader = csv.reader(rtext,delimiter=';')
+			for row in csvReader:
+				if row[0]==indText:
+					rand1 = random.randint(5,len(row)-1)
+					if rand1 %2 == 0:
+						rand1 +=1
+					rand2 = rand1+1
+					return row[1],row[2],row[3],row[4],row[rand1].replace('@','\n'),row[rand2].replace('@','\n')
+					
 def fetchVign():
 	starts = []
 	startdest = []
@@ -38,9 +49,13 @@ def writeStrip(story):
 			vign = Image.open(indVign)
 			addtext = ImageDraw.Draw(vign)
 			fnt = ImageFont.truetype("ubuntu.ttf",16)
-			textVign = fetchText(indVign)
-			print(textVign)
-			addtext.text((268,77),textVign,fill="#000000",font=fnt)
+			if indVign[0] == 'A':
+				textVign = fetchText(indVign)
+				addtext.multiline_text((int(textVign[0]),int(textVign[1])),textVign[2],fill="#000000",font=fnt,align="center")
+			else:
+				textVign = fetch2Text(indVign)
+				addtext.multiline_text((int(textVign[0]),int(textVign[1])),textVign[4],fill="#000000",font=fnt,align="center")
+				addtext.multiline_text((int(textVign[2]),int(textVign[3])),textVign[5],fill="#000000",font=fnt,align="center")
 			vign.show()
 
 if __name__ == "__main__":
