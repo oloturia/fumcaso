72e9a43123bd4be27519bcdb1f17911e8a076f05
oloturia <oloturia@gmail.com>
2019-09-17 00:20:30 +0200

    Added merging of cartoons. Allows cartoons with no text.

--- randstrip.py
+++ randstrip.py
@@ -9,19 +9,25 @@ def fetchText(indText):
 	with open("rtext.csv") as rtext:
 		csvReader = csv.reader(rtext,delimiter=';')
 		for row in csvReader:
-			if row[0]==indText:
-				return row[1],row[2],row[random.randint(3,len(row)-1)].replace('@','\n')
-				
+			if len(row)>1:
+				if row[0]==indText:
+					return row[1],row[2],row[random.randint(3,len(row)-1)].replace('@','\n')
+				else:
+					return 0
+					
 def fetch2Text(indText):
 		with open("r2text.csv") as rtext:
 			csvReader = csv.reader(rtext,delimiter=';')
 			for row in csvReader:
-				if row[0]==indText:
-					rand1 = random.randint(5,len(row)-1)
-					if rand1 %2 == 0:
-						rand1 +=1
-					rand2 = rand1+1
-					return row[1],row[2],row[3],row[4],row[rand1].replace('@','\n'),row[rand2].replace('@','\n')
+				if len(row)>1:
+					if row[0]==indText:
+						rand1 = random.randint(5,len(row)-1)
+						if rand1 %2 == 0:
+							rand1 +=1
+						rand2 = rand1+1
+						return row[1],row[2],row[3],row[4],row[rand1].replace('@','\n'),row[rand2].replace('@','\n')
+				else:
+					return 0
 					
 def fetchVign():
 	starts = []
@@ -40,6 +46,7 @@ def fetchVign():
 		if story[nvign] == "B00":
 			story[nvign] += "."
 			story[nvign] += str(random.randint(0,2))
+		story[nvign]+=".png"
 		nvign +=1
 	return story
 		
@@ -48,6 +55,7 @@ def addThing(vign):
 	pass
 
 def writeStrip(story):
+	strip = []
 	for indVign in story:
 		if indVign!="000":
 			vign = Image.open(indVign)
@@ -55,19 +63,26 @@ def writeStrip(story):
 			fnt = ImageFont.truetype("ubuntu.ttf",16)
 			if indVign[0] == 'A':
 				textVign = fetchText(indVign)
-				addtext.multiline_text((int(textVign[0]),int(textVign[1])),textVign[2],fill="#000000",font=fnt,align="center")
+				if textVign !=0:
+					addtext.multiline_text((int(textVign[0]),int(textVign[1])),textVign[2],fill="#000000",font=fnt,align="center")
 			else:
 				textVign = fetch2Text(indVign)
-				addtext.multiline_text((int(textVign[0]),int(textVign[1])),textVign[4],fill="#000000",font=fnt,align="center")
-				addtext.multiline_text((int(textVign[2]),int(textVign[3])),textVign[5],fill="#000000",font=fnt,align="center")
+				if textVign!=0:
+					addtext.multiline_text((int(textVign[0]),int(textVign[1])),textVign[4],fill="#000000",font=fnt,align="center")
+					addtext.multiline_text((int(textVign[2]),int(textVign[3])),textVign[5],fill="#000000",font=fnt,align="center")
 			obj = addThing(indVign)
 			if obj!=(0,0):
 				#TODO
 				pass
-				
-			vign.show()
+			strip.append(vign)
+	image = Image.new('RGB',(2400,500))
+	xshift=0
+	for vign in strip:
+		image.paste(vign,(xshift,0))
+		xshift += 600
+	return image
 
 if __name__ == "__main__":
-	#story = fetchVign()
-	#writeStrip(story)
-	writeStrip(["A00.png"])
+	story = fetchVign()
+	finalStrip = writeStrip(story)
+	finalStrip.show()
