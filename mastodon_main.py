453caed72f567fa592d5182121ec0377781ec41c
oloturia <oloturia@gmail.com>
2019-09-23 15:33:03 +0200

    Fixed mastodon bot

--- mastodon_main.py
+++ mastodon_main.py
@@ -2,39 +2,19 @@
 from randstrip import createStrip
 from mastodon import Mastodon
 import os.path
-		
+
+API_URL = "https://botsin.space"
+	
 if __name__ == "__main__":
 	with open("createapp") as f:
 		createapp = f.readlines()
 	createapp = [x.strip() for x in createapp]
-	app = createapp[0]
-	api_base_url_cr = createapp[1]
-	to_file_cr = createapp[2]
-	email = createapp[3]
-	pw = createapp[4]
-	if os.path.exists(content[2]) == False:
-		Mastodon.create_app(
-			app,
-			api_base_url = api_base_url_cr,
-			to_file = to_file_cr
-		)
-	mastodon = Mastodon(
-		client_id = to_file_cr,
-		api_base_url = api_base_url_cr
-	)
-	mastodon.log_in(
-		email,
-		pw,
-		to_file = to_file_cr
-	)
-	
-	mastodon = Mastodon(
-		access_token = to_file_cr
-		api_base_url = api_base_url_cr
-	)
+	TOKEN = createapp[0]
+	mastodon = Mastodon(access_token = TOKEN,api_base_url = API_URL)
 	status = createStrip("mastodon.png")
 	if status == 0:
-		mastodon.media_post("mastodon.png","image/png")
+		new_strip = mastodon.media_post("mastodon.png","image/png")
+		mastodon.status_post("Nuova striscia",media_ids=new_strip)
 	else:
 		print("error creating image\n")
-		print status
+		print(status)
