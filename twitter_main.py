28d6f3cc3b708dbf2f814dadc1743850c61e9e02
oloturia <oloturia@gmail.com>
2019-10-05 23:25:52 +0200

    Added a twitter bot

---
+++ twitter_main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import tweepy
+from randstrip import createStrip
+import os
+
+fileDir = os.path.dirname(os.path.abspath(__file__))
+fileDir = fileDir +"/"
+
+if __name__ == "__main__":
+	status = createStrip("twitter.png")
+	if status == 0:
+		with open(fileDir+"twitter_token") as f:
+			tokens = f.readlines()
+		tokens = [x.strip() for x in tokens]
+		auth = tweepy.OAuthHandler(tokens[0],tokens[1])
+		auth.set_access_token(tokens[2],tokens[3])
+		api = tweepy.API(auth)
+		try:
+			api.verify_credentials()
+			print("Auth OK")
+			api.update_with_media(fileDir+"twitter.png","Generatore automatico di strip. Striscia di oggi.")
+		except:
+			print("Auth error")
+	else:
+		print("Error creating image\n")
+		print(status)
