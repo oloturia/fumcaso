e239f229915735c753c737387833c6f17cb2e865
oloturia <5429234+oloturia@users.noreply.github.com>
2022-05-02 02:02:22 +0200

    bug

--- mastodon_replier.py
+++ mastodon_replier.py
@@ -18,7 +18,7 @@ class stripListener(StreamListener):
 			content = notification["status"]["content"]
 			if content.find("help") != -1:
 				mastodon.status_post("Hello "+account+" just send me a message with 'new strip' and the desired profile. Try with 'oloturia' (Italian), 'oloeng' (broken English) or 'olofra' (French). If not specified, Italian will be selected as default.",visibility="direct")
-			elif content.find("strip") != -1:
+			elif content.find("new strip") != -1:
 				if content.find("oloeng") != -1:
 					profile = "oloeng"
 				elif content.find("olofra") != -1:
@@ -33,7 +33,7 @@ class stripListener(StreamListener):
 if __name__ == "__main__":
 	with open(fileDir+"/config.json") as f:
 		config = json.load(f)
-	with open(config["mastodonListenerToken"]) as f:
+	with open(fileDir+config["mastodonListenerToken"]) as f:
 		createapp = f.readlines()
 	createapp = [x.strip() for x in createapp]
 	TOKEN = createapp[0]
