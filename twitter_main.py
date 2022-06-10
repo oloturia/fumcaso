9888b34f33d2cd41364539958ed96a3b629ddc5b
Oloturia <oloturia@gmail.com>
2022-06-10 17:20:43 +0200

    missing )

--- twitter_main.py
+++ twitter_main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 				api.verify_credentials()
 				api.update_with_media(config["saveLocation"]+config["filename"],config["text"])
 				published = True
-				os.remove(config["saveLocation"]+config["filename"]
+				os.remove(config["saveLocation"]+config["filename"])
 			except:
 				continue
 			break
